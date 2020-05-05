e4ea69aa844d6e351592d2cb9e1c17a6286adf7f
Alex <lzlin@mail.ru>
2020-05-05 13:52:43 +1000

    создает контроллер карточки фильма

---
+++ src/controllers/movie.js
@@ -0,0 +1,12 @@
+
+import FilmCardComponent from "../components/film-card";
+import FilmDetailComponent from "../components/film-detail";
+import {render, remove, RenderPosition} from "../utils/render";
+
+export default class MovieController {
+  constructor(container) {
+  }
+
+  render(film) {
+  }
+}
