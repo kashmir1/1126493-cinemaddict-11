b33f540b928792d75eeb7dd87c29d497dbdc2c59
Alex <lzlin@mail.ru>
2020-05-21 23:14:26 +1000

    описывает функцию фильтрации

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,3 +1,26 @@
-export const getMoviesByFilter = (movies) => {
-  return movies;
+import {FilterType} from "../consts";
+
+export const getWatchlistMovies = (movies) => {
+  return movies.filter((movie) => movie.watchlist);
+};
+
+export const getWatchedMovies = (movies) => {
+  return movies.filter((movie) => !movie.alreadyWatched);
+};
+
+export const getFavoriteMovies = (movies) => {
+  return movies.filter((movie) => movie.favorite);
+};
+
+export const getMoviesByFilter = (movies, filterType) => {
+  switch (filterType) {
+    case FilterType.WATCHLIST:
+      return getWatchlistMovies(movies);
+    case FilterType.HISTORY:
+      return getWatchedMovies(movies);
+    case FilterType.FAVORITES:
+      return getFavoriteMovies(movies);
+    default:
+      return movies;
+  }
 };
