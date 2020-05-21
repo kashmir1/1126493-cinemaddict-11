de8621f2a31d0b554034413a261ce3652cd3bcbe
Alex <lzlin@mail.ru>
2020-05-21 22:58:00 +1000

    описывает логику фильтрации

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,10 +1,15 @@
 import AbstractComponent from "./abstract-component";
 
-const createFilterMarkup = (name, count, url) => {
+const MAIN_FILTER = `All movies`;
+const getFilterName = (element) => element.innerText.replace(/[0-9]/g, ``);
 
+const createFilterMarkup = (name, count, url, checked) => {
+  const isMainFilter = name === MAIN_FILTER;
   return (
     `
-     <a href="#${url}" class="main-navigation__item">${name} <span class="main-navigation__item-count">${count}</span></a>
+     <a href="#${url}" class="main-navigation__item ${checked ? `main-navigation__item--active` : ``}">
+     ${name} ${isMainFilter ? `` : `<span class="main-navigation__item-count">${count}</span>`}
+     </a>
     `
   );
 };
@@ -34,5 +39,31 @@ export default class Navigation extends AbstractComponent {
 
   getTemplate() {
     return createNavigation(this._filters);
+  }
+
+  setFilterChangeHandler(handler) {
+    this.getElement().querySelector(`.main-navigation__items`)
+      .addEventListener(`click`, (evt) => {
+        evt.preventDefault();
+        const target = evt.target;
+        const tag = target.tagName;
+
+        /* Проверка на наличие класса нужна для того, чтобы фильтрация не выполнялась при клике на уже активный фильтр */
+        if ((tag !== `A` && tag !== `SPAN`) || (target.classList.contains(`main-navigation__item--active`) || target.parentElement.classList.contains(`main-navigation__item--active`))) {
+          return;
+        }
+
+        const filterName = tag === `A` ? getFilterName(target) : getFilterName(target.parentElement);
+
+        this.getElement().querySelector(`.main-navigation__item--active`).classList.remove(`main-navigation__item--active`);
+
+        if (tag === `A`) {
+          target.classList.add(`main-navigation__item--active`);
+        } else {
+          target.parentElement.classList.add(`main-navigation__item--active`);
+        }
+
+        handler(filterName);
+      });
   }
 }
