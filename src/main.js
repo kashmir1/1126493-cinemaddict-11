aed70b99471f06ca6435c39ac2524dbab3d166b0
Alex <lzlin@mail.ru>
2020-05-29 13:42:30 +1000

    выводит прелоадер

--- src/main.js
+++ src/main.js
@@ -17,36 +17,42 @@ const moviesModel = new MoviesModel();
 const headerElem = document.querySelector(`.header`);
 const mainElem = document.querySelector(`.main`);
 
+
+const filterController = new FilterController(mainElem, moviesModel, () => {
+  statisticsComponent.hide();
+  pageController.show();
+});
+filterController.render();
+
+const statisticsComponent = new StatisticsComponent(moviesModel.getAllMovies().filter(({userDetails: {alreadyWatched}}) => alreadyWatched));
+render(mainElem, statisticsComponent);
+statisticsComponent.hide();
+
 const movieListPreloaderComponent = new MovieListPreloaderComponent();
 render(mainElem, movieListPreloaderComponent, RenderPosition.BEFOREEND);
 
+const pageController = new PageController(mainElem, moviesModel, api);
+pageController.render();
+
+filterController.setOnStatsClick(() => {
+  pageController.hide();
+  statisticsComponent.show(moviesModel.getAllMovies().filter(({userDetails: {alreadyWatched}}) => alreadyWatched));
+});
+
 api.getMovies()
   .then((movies) => {
-    const watchedMovies = movies.filter(({userDetails: {alreadyWatched}}) => alreadyWatched);
     moviesModel.setMovies(movies);
-    render(headerElem, new UserRankComponent(watchedMovies.length), RenderPosition.BEFOREEND);
 
-    const filterController = new FilterController(mainElem, moviesModel, () => {
-      statisticsComponent.hide();
-      pageController.show();
-    });
-    filterController.render();
+    const watchedMovies = moviesModel.getAllMovies().filter(({userDetails: {alreadyWatched}}) => alreadyWatched);
+    render(headerElem, new UserRankComponent(watchedMovies.length));
 
     const footerElement = document.querySelector(`.footer`);
     const footerStatisticsElement = footerElement.querySelector(`.footer__statistics`);
-    const pageController = new PageController(mainElem, moviesModel, api);
+    filterController.render();
+
     remove(movieListPreloaderComponent);
-
-    render(footerStatisticsElement, new FooterStatisticsComponent(movies.length), RenderPosition.BEFOREEND);
-    pageController.render(movies);
-
-
-    const statisticsComponent = new StatisticsComponent(watchedMovies);
+    pageController.render();
+    render(footerStatisticsElement, new FooterStatisticsComponent(moviesModel.getMovies().length), RenderPosition.BEFOREEND);
     render(mainElem, statisticsComponent, RenderPosition.BEFOREEND);
     statisticsComponent.hide();
-
-    filterController.setOnStatsClick(() => {
-      pageController.hide();
-      statisticsComponent.show(moviesModel.getAllMovies().filter(({userDetails: {alreadyWatched}}) => alreadyWatched));
-    });
   });
